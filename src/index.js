155e7ab36ff930a73e0e770b3ac2b963cd3dfba9
Luís <59730229+d4k37@users.noreply.github.com>
2021-04-28 14:42:35 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,13 +5,5 @@ const app = express();
 app.use(express.json());
 
 
-app.get("/", (request, response)=>{
-    
-    return response.json([
-        "Curso1",
-        "Curso2",
-        "Curso3"
-    ]);
-    
-});
+
 app.listen(3334)
