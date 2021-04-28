62917e61ea5d1802f037dd458d0e40abf39cc055
Luís <59730229+d4k37@users.noreply.github.com>
2021-04-28 15:48:09 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
+const { request, response } = require('express');
 const express =require('express');
+const {v4 : uuidv4} = require("uuid");
+
 const app = express();
+app.use(express.json());
+const customers = [];
 
 
+/**
+ * cpf -string
+ * name - string
+ * id - uuid
+ * statement - array []
+ */
+app.post("/account", (request, response)=>{
+    const {cpf, name} = request.body;
+   
+    const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf)
 
 
+    if(customerAlreadyExists){
+        return response.status(400).json({error: "JÁ EXISTE"})
+    }
+
+   customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    });
+    return response.status(201).send();
+})
+
+app.get("/statement", (request, response) =>{
+    
+})
 
 app.listen(3334)
