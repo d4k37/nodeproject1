91d1fb57ec05db2c7ca82e0fe2d7f9efe39bdcde
Luís <59730229+d4k37@users.noreply.github.com>
2021-04-29 15:14:28 -0300

    up final

--- src/index.js
+++ src/index.js
@@ -108,4 +108,20 @@ app.get("/account", verifyIfExistsAccountCPF, (request, response)=>{
     return response.json(customer);
 });
 
+app.delete("/account", verifyIfExistsAccountCPF, (request, response)=>{
+    const {customer} = request;
+
+    //splice
+
+    customers.splice(customer, 1);
+    return response.status(200).json(customers);
+});
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response)=>{
+    const {customer} = request;
+
+    const balance = getBalance(customer.statement);
+
+    return response.json(balance);
+})
 app.listen(3334)
